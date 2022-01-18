a0fe5ad90f19b4f1944a85aacf21cef1096af817
njager <59936380+njager@users.noreply.github.com>
2022-01-17 19:21:58 -0500

    did the things

---
+++ FMODKarting/Assets/Karting/Scripts/KartSystems/KartAudio/ArcadeEngineAudio.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace KartGame.KartSystems
+{
+    /// <summary>
+    /// This class produces audio for various states of the vehicle's movement.
+    /// </summary>
+    public class ArcadeEngineAudio : MonoBehaviour
+    {
+        public float minRPM = 0;
+        public float maxRPM = 5000;
+        ArcadeKart arcadeKart;
+
+        void Awake()
+        {
+            arcadeKart = GetComponentInParent<ArcadeKart>();
+        }
+
+        void Update()
+        {
+            float kartSpeed = arcadeKart != null ? arcadeKart.LocalSpeed() : 0;
+            // set RPM value for the FMOD event
+            float effectiveRPM = Mathf.Lerp(minRPM, maxRPM, kartSpeed);
+            var emitter = GetComponent<FMODUnity.StudioEventEmitter>();
+            emitter.SetParameter("RPM", effectiveRPM);
+        }
+    }
+}
